ecc1e214e4a0b619b9f4774b1aa491b1be3b51be
jieqianzhang <zhang.jieqian@gmail.com>
2016-01-15 18:49:57 +0800

    update

---
+++ cities.js
@@ -0,0 +1,70 @@
+var arcdata = [
+		{
+			sourceLocation: [-112.0737951, 33.44818315],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "Phoenix",
+			index: 12
+		},
+		{
+			sourceLocation: [-118.2433436, 34.05223822],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "Los Angeles",
+			index: 5
+
+		},
+		{
+			sourceLocation: [-117.1638723, 32.71881381],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "San Diego",
+			index: 7
+		},
+		{
+			sourceLocation: [-115.2422959, 36.23302567],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "Las Vegas",
+			index: 8
+		},
+		{
+			sourceLocation: [-122.6703581, 45.52386617],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "Portland",
+			index: 5
+		},
+        {
+            sourceLocation: [-122.347306, 47.62101417],
+            targetLocation: [-122.4194155, 37.7749295],
+            city: "Seatle",
+            index: 9
+        },
+		{
+			sourceLocation: [-104.9881051, 39.75508869],
+			targetLocation: [-122.4194155, 37.7749295],
+			city: "Denver",
+			index: 8
+		},
+        {
+            sourceLocation: [-93.26434597, 44.97747893],
+            targetLocation: [-122.4194155, 37.7749295],
+            city: "Minneapolis",
+            index: 9
+        },
+        {
+            sourceLocation: [-94.58364412, 39.0904308],
+            targetLocation: [-122.4194155, 37.7749295],
+            city: "Kansas City",
+            index: 14
+        },
+        {
+            sourceLocation: [-90.1983923, 38.62748431],
+            targetLocation: [-122.4194155, 37.7749295],
+            city: "St. Louis",
+            index: 10
+        },
+        {
+            sourceLocation: [-96.79711092, 32.78107836],
+            targetLocation: [-122.4194155, 37.7749295],
+            city: "Dallas",
+            index: 12
+        }
+	]
+
