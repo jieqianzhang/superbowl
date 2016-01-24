06715daf7e2bd4f03f3b3316bbd411d2b930724d
jieqianzhang <zhang.jieqian@gmail.com>
2016-01-24 13:59:33 -0800

    update

--- cities.js
+++ cities.js
@@ -3,33 +3,31 @@ var arcdata = [{
     targetLocation: [-122.4194155, 37.7749295],
     city: "Phoenix",
     index: 12,
+    tooltip: "Lorem Ipsum is 1",
     gradient: [{
         offset: "30%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 1."
+    }]
 }, {
     sourceLocation: [-118.2433436, 34.05223822],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Los Angeles",
     index: 5,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "20%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 2. "
-
-
+    }]
 }, {
     sourceLocation: [-117.1638723, 32.71881381],
     targetLocation: [-122.4194155, 37.7749295],
     city: "San Diego",
     index: 7,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "22%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 3. "
+    }]
 
 
 }, {
@@ -37,79 +35,79 @@ var arcdata = [{
     targetLocation: [-122.4194155, 37.7749295],
     city: "Las Vegas",
     index: 8,
+    tooltip: "Lorem Ipsum is 3",
     gradient: [{
         offset: "30%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 4. "
+    }]
 }, {
     sourceLocation: [-122.6703581, 45.52386617],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Portland",
     index: 5,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "20%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 5. "
+    }]
 }, {
     sourceLocation: [-122.347306, 47.62101417],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Seatle",
     index: 9,
+    tooltip: "Seattle Seahawks fans are the #1 most loyal NFL team in America. Throughout the NFL season, they searched the most for flights to the Super Bowl",
     gradient: [{
         offset: "20%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 6. "
+    }]
 }, {
     sourceLocation: [-104.9881051, 39.75508869],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Denver",
     index: 8,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "40%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 7. "
+    }]
 }, {
     sourceLocation: [-93.26434597, 44.97747893],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Minneapolis",
     index: 9,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "50%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 8. "
+    }]
 }, {
     sourceLocation: [-94.58364412, 39.0904308],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Kansas City",
     index: 14,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "50%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 9. "
+    }]
 }, {
     sourceLocation: [-90.1983923, 38.62748431],
     targetLocation: [-122.4194155, 37.7749295],
     city: "St. Louis",
     index: 10,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "50%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 10. "
+    }]
 }, {
     sourceLocation: [-96.79711092, 32.78107836],
     targetLocation: [-122.4194155, 37.7749295],
     city: "Dallas",
     index: 12,
+    tooltip: "Lorem Ipsum is 2",
     gradient: [{
         offset: "50%",
         color: "lightblue"
-    }],
-    text: "Lorem Ipsum is 11. "
+    }]
 }]
